bb0fb30e8b220b390d0a41245b7349f6b9c3fd0d
SoporteCSR <soportetic.csr@gmail.com>
2025-08-18 18:13:58 -0500

    go

--- app/Repositories/MedicalRecordRequestRepository.php
+++ app/Repositories/MedicalRecordRequestRepository.php
@@ -16,6 +16,41 @@ class MedicalRecordRequestRepository extends BaseRepository implements MedicalRe
     }
 
     /**
+     * Override getAll to order by request_date descending.
+     *
+     * @param array $relations
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getAll(array $relations = [])
+    {
+        if (!empty($relations)) {
+            return $this->model::with($relations)->orderBy('request_date', 'desc')->get();
+        }
+        return $this->model::orderBy('request_date', 'desc')->get();
+    }
+
+    /**
+     * Override getDateRange to use request_date instead of created_at.
+     *
+     * @param string $from
+     * @param string $to
+     * @param array $relations
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getDateRange($from = null, $to = null, $relations = [])
+    {
+        if (!empty($relations)) {
+            return $this->model::with($relations)
+                ->whereBetween('request_date', [$from, $to])
+                ->orderBy('request_date', 'desc')
+                ->get();
+        }
+        return $this->model->whereBetween('request_date', [$from, $to])
+            ->orderBy('request_date', 'desc')
+            ->get();
+    }
+
+    /**
      * Search records by medical record number using LIKE.
      *
      * @param string $medicalRecordNumber
@@ -24,7 +59,7 @@ class MedicalRecordRequestRepository extends BaseRepository implements MedicalRe
     public function searchByMedicalRecordNumber($medicalRecordNumber)
     {
         return $this->model->where('medical_record_number', 'LIKE', '%' . $medicalRecordNumber . '%')
-            ->orderBy('created_at', 'desc')
+            ->orderBy('request_date', 'desc')
             ->get();
     }
 }
